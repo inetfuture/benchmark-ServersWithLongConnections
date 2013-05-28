db63a1e9e6a77ff31c9e65400ceb43efda16c537
inetfuture(Aaron Wang) <inetfuture@gmail.com>
2013-05-28 13:03:56 +0800

    Change server to be a echo server.

--- client.js
+++ client.js
@@ -2,6 +2,10 @@ var net = require('net');
 
 var client = net.connect({ port: 6666, host: 'localhost' }, function () {
   console.log('Connected');
+
+  setInterval(function () {
+    client.write('hello world!\n');
+  }, 1000);
 });
 
 client.on('data', function (chunk) {
--- servers/go/server.go
+++ servers/go/server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+  "bufio"
   "net"
   "os"
-  "time"
 )
 
 const (
@@ -30,13 +30,53 @@ func main() {
 }
 
 func handler(conn net.Conn) {
-  for  {
-    towrite := []byte("Welcome to the Server!!")
-    _, err := conn.Write(towrite[:])
-    if err != nil {
-        println("Write error: ",err.Error())
+  b := bufio.NewReader(conn)
+  for {
+    line, err := b.ReadBytes('\n')
+    if err != nil { // EOF, or worse
+      break
     }
+    conn.Write(line)
+  }
+}
+package main
 
-    time.Sleep(3 * time.Second)
+import (
+  "bufio"
+  "net"
+  "os"
+)
+
+const (
+  RECV_BUF_LEN = 1024
+)
+
+func main() {
+  listener, err := net.Listen("tcp", "0.0.0.0:6666")
+  if err != nil {
+    println("Listen error:", err.Error())
+    os.Exit(1)
+  }
+
+  println("Go server is listening on port:", 6666)
+
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      println("Accept error :", err.Error())
+      return
+    }
+    go handler(conn)
+  }
+}
+
+func handler(conn net.Conn) {
+  b := bufio.NewReader(conn)
+  for {
+    line, err := b.ReadBytes('\n')
+    if err != nil { // EOF, or worse
+      break
+    }
+    conn.Write(line)
   }
 }
