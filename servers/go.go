7e9d85d924d8dbe490d1fb28b6899df335cd137f
inetfuture(Aaron Wang) <inetfuture@gmail.com>
2013-06-04 18:52:12 +0800

    Format go coding style.

--- servers/go.go
+++ servers/go.go
@@ -1,82 +1,41 @@
 package main
 
 import (
-  "bufio"
-  "net"
-  "os"
+	"bufio"
+	"net"
+	"os"
 )
 
 const (
-  RECV_BUF_LEN = 1024
+	RECV_BUF_LEN = 1024
 )
 
 func main() {
-  listener, err := net.Listen("tcp", "0.0.0.0:6666")
-  if err != nil {
-    println("Listen error:", err.Error())
-    os.Exit(1)
-  }
+	listener, err := net.Listen("tcp", "0.0.0.0:6666")
+	if err != nil {
+		println("Listen error:", err.Error())
+		os.Exit(1)
+	}
 
-  println("Go server is listening on port:", 6666)
+	println("Go server is listening on port:", 6666)
 
-  for {
-    conn, err := listener.Accept()
-    if err != nil {
-      println("Accept error :", err.Error())
-      return
-    }
-    go handler(conn)
-  }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			println("Accept error :", err.Error())
+			return
+		}
+		go handler(conn)
+	}
 }
 
 func handler(conn net.Conn) {
-  b := bufio.NewReader(conn)
-  for {
-    line, err := b.ReadBytes('\n')
-    if err != nil { // EOF, or worse
-      break
-    }
-    conn.Write(line)
-  }
-}
-package main
-
-import (
-  "bufio"
-  "net"
-  "os"
-)
-
-const (
-  RECV_BUF_LEN = 1024
-)
-
-func main() {
-  listener, err := net.Listen("tcp", "0.0.0.0:6666")
-  if err != nil {
-    println("Listen error:", err.Error())
-    os.Exit(1)
-  }
-
-  println("Go server is listening on port:", 6666)
-
-  for {
-    conn, err := listener.Accept()
-    if err != nil {
-      println("Accept error :", err.Error())
-      return
-    }
-    go handler(conn)
-  }
-}
-
-func handler(conn net.Conn) {
-  b := bufio.NewReader(conn)
-  for {
-    line, err := b.ReadBytes('\n')
-    if err != nil { // EOF, or worse
-      break
-    }
-    conn.Write(line)
-  }
+	b := bufio.NewReader(conn)
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil { // EOF, or worse
+			break
+		}
+		conn.Write(line)
+	}
 }
